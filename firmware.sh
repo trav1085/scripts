ec2a27c08913741d787d57fb345319b5ddfa510a
Travis Nowicki <60045854+trav1085@users.noreply.github.com>
2025-09-04 20:20:56 -0700

    lazily removed code that requires stock backup

--- firmware.sh
+++ firmware.sh
@@ -861,37 +861,8 @@ function backup_firmware()
 to continue.  This is non-destructive, but it is best to ensure no other
 USB/SD devices are connected. "
 
-    if ! list_usb_devices; then
-        backup_fail "No USB devices available to store firmware backup."
-        return 1
-    fi
-
-    usb_dev_index=""
-    while [[ "$usb_dev_index" = "" || ($usb_dev_index -le 0 && $usb_dev_index -gt $num_usb_devs) ]]; do
-        read -rep "Enter the number for the device to be used for firmware backup: " usb_dev_index
-        if [[ "$usb_dev_index" = "" || ($usb_dev_index -le 0 && $usb_dev_index -gt $num_usb_devs) ]]; then
-            echo -e "Error: Invalid option selected; enter a number from the list above."
-        fi
-    done
-
-    usb_device="${usb_devs[${usb_dev_index}-1]}"
-    mkdir /tmp/usb > /dev/null 2>&1
-    if ! mount -o rw "${usb_device}" /tmp/usb > /dev/null 2>&1; then
-        if ! mount -o rw "${usb_device}1" /tmp/usb > /dev/null 2>&1; then
-            backup_fail "USB backup device failed to mount; cannot proceed. Ensure your USB is FAT32-formatted and try again."
-            return 1
-        fi
-    fi
-    backupname="stock-firmware-${boardName}-$(date +%Y%m%d).rom"
-    echo_yellow "\nSaving firmware backup as ${backupname}"
-    if ! cp /tmp/bios.bin /tmp/usb/${backupname}; then
-        backup_fail "Failure copying stock firmware to USB; cannot proceed."
-        return 1
-    fi
-    sync
-    umount /tmp/usb > /dev/null 2>&1
-    rmdir /tmp/usb
-    echo_green "Firmware backup complete. Remove the USB stick and press [Enter] to continue."
+    
+    echo_green "skipping firmware backup! Remove the USB stick and press [Enter] to continue."
     read -rep ""
 }
 
